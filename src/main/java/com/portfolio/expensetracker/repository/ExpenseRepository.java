b6a8c993ef5cef3d4bf9f30ad254ef038e649abc
Stefano <stefanobellan94@gmail.com>
2026-01-22 16:58:29 +0100

    feat: creazione dell'interfaccia ExpenseRepository con metodi di query personalizzati

---
+++ src/main/java/com/portfolio/expensetracker/repository/ExpenseRepository.java
@@ -0,0 +1,35 @@
+package com.portfolio.expensetracker.repository;
+
+import com.portfolio.expensetracker.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Repository per la gestione della persistenza delle entità Expense
+ * Estende JpaRepository per ereditare i metodi CRUD standard (save, findAll, delete, ecc.).
+ * @author Stefano Bellan
+ */
+@Repository // Indico a Spring che questa interfaccia è un componente DAO
+public interface ExpenseRepository extends JpaRepository<Expense, Long>{
+
+    /**
+     * Trova tutte le spese appartenenti a una specifica categoria.
+     * <p>Spring Data JPA genererà automaticamente le query </p>
+     * <p>SELECT * FROM expenses WHERE category = ? </p>
+     * @param category La categoria da cercare
+     * @return Lista di spese trovate
+     */
+    List<Expense> findByCategory(String category);
+
+    /**
+     * Trova tutte le spese effettuate tra due date.
+     * <p> Genera SQL: SELECT * FROM expenses WHERE date BETWEEN ? AND ? </p>
+     * @param startDate Data di inizio
+     * @param endDate Data di fine
+     * @return Lista di spese nel range temporale
+     */
+    List<Expense> findByDateBetween(LocalDate startDate, LocalDate endDate);
+}
